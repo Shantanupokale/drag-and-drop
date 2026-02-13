ec019a206d8d36d08c18541296762b3277983e08
Shantanu Pokale <shantanupokale@gmail.com>
2026-02-13 12:50:25 +0530

    feat: added handler logic for drag and drop

--- src/components/Dndbox.jsx
+++ src/components/Dndbox.jsx
@@ -1,5 +1,5 @@
 // dndbox.jsx
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 const intialstate = {
   Todo: ["read book", "write code", "drink water", "fix merge requests"],
@@ -8,17 +8,61 @@ const intialstate = {
 };
 
 const DndBox = () => {
-  const [data, newData] = useState(intialstate);
+  const [data, setData] = useState(intialstate);
+
+  const dragItem = useRef();
+  const dragColumn = useRef();
+
+  const handleDragStart = (e, idx, column) => {
+    dragItem.current = idx;
+    dragColumn.current = column;
+    e.target.style.opacity = "0.5";
+    console.log(dragItem);
+    console.log(dragColumn);
+  };
+  const handleDragEnd = (e) => {
+    e.target.style.opacity = "1";
+  };
+
+  const handleDrop = (e, targetcolumn) => {
+    const item = dragItem.current;
+    const sourcecolumn = dragColumn.current;
+    setData((prev) => {
+      const newData = { ...prev };
+      newData[sourcecolumn] = newData[sourcecolumn].filter(
+        (_, idx) => idx !== item,
+      );
+      newData[targetcolumn] = [
+        ...newData[targetcolumn],
+        prev[sourcecolumn][item],
+      ];
+      return newData;
+    });
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <div className=" flex flex-row justify-around  ">
-      {Object.keys(data).map((columns) => {
+      {Object.keys(data).map((column) => {
         return (
-          <div className=" mt-8 py-8  px-4 sm:px-8 border border-black bg-gray-200 ">
-            <h1 className="text-2xl p-2 font-semibold text-center">{columns}</h1>
-            {data[columns].map((item, idx) => {
+          <div
+            className=" mt-8 py-8  px-4 sm:px-8 border border-black bg-gray-200 "
+            onDrop={(e) => handleDrop(e, column)}
+            onDragOver={handleDragOver}
+          >
+            <h1 className="text-2xl p-2 font-semibold text-center">{column}</h1>
+            {data[column].map((item, idx) => {
               return (
-                <div className="border p-4 mb-1 bg-white text-center" key={idx}>
+                <div
+                  className={`border p-4 mb-1 text-center ${ column === "Todo" ? "bg-white"  : column === "InProgress"  ? "bg-yellow-200" : "bg-green-300" }`}
+                  key={idx}
+                  onDragStart={(e) => handleDragStart(e, idx, column)}
+                  onDragEnd={handleDragEnd}
+                  draggable
+                >
                   {item}
                 </div>
               );
