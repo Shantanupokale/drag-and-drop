df7bc56e4f839e8dac110b760c4c566482f86ed7
Shantanu Pokale <shantanupokale@gmail.com>
2026-02-13 18:26:44 +0530

    fix : minor file name change

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import DndBox from './components/dndbox'
+import DndBox from './components/Dndbox'
 
 function App() {
   const [count, setCount] = useState(0)
