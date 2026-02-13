e5d52bca269e3f6b3a3fab759894e929bd1ac56b
Shantanu Pokale <shantanupokale@gmail.com>
2026-02-13 11:35:51 +0530

    added some css

--- src/components/Dndbox.jsx
+++ src/components/Dndbox.jsx
@@ -1,35 +1,33 @@
 // dndbox.jsx
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-const intialstate ={
-    Todo: ["read book","write code","drink water","fix merge requests"],
-    InProgress:["working on dnd project", "learning nodejs"],
-    Completed:["js course lectures","setting-up project"],
+const intialstate = {
+  Todo: ["read book", "write code", "drink water", "fix merge requests"],
+  InProgress: ["working on dnd project", "learning nodejs"],
+  Completed: ["js course lectures", "setting-up project"],
 };
 
 const DndBox = () => {
-  const[data,newData] = useState(intialstate);
-
+  const [data, newData] = useState(intialstate);
 
   return (
-    
-    <div>
-        {Object.keys(data).map((todos) => {
-            return (
-                <div className='flex flex-col bg-green'>
-                    
-                        {todos}
-                        
-                    {data[todos].map((item,idx) => {
-                        return <div key={idx}>{item}</div>
-                    })}
-
-                    
+    <div className=" flex flex-row justify-around  ">
+      {Object.keys(data).map((columns) => {
+        return (
+          <div className=" mt-8 py-8  px-4 sm:px-8 border border-black bg-gray-200 ">
+            <h1 className="text-2xl p-2 font-semibold text-center">{columns}</h1>
+            {data[columns].map((item, idx) => {
+              return (
+                <div className="border p-4 mb-1 bg-white text-center" key={idx}>
+                  {item}
                 </div>
-            )
-        } )}
+              );
+            })}
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default DndBox
+export default DndBox;
